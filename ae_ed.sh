0dbf5d3a69e06e0ef4f3af3aa9c5442e63a827bb
liubin13720 <liubin13720@ipalfish.com>
2021-06-08 16:24:12 +0800

    modify

--- ae_ed.sh
+++ ae_ed.sh
@@ -1,5 +1,5 @@
-python ae_ed.py \
+CUDA_VISIBLE_DEVICES=2 python ae_ed.py \
     metalwoz-v1 \
     LAEDBlisCorpus \
-    vocabs/maluuba.json \
+    vocabs/maluuba.json
 
--- st_ed.sh
+++ st_ed.sh
@@ -1,6 +1,4 @@
-python st_ed.py \
+CUDA_VISIBLE_DEVICES=1 python st_ed.py \
     metalwoz-v1 \
     LAEDBlisCorpus \
-    vocabs/maluuba.json \
-   --exlcude_domains ... \
-   --y_size 10
+    vocabs/maluuba.json
